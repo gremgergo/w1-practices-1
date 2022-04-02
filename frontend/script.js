dc92bde633fad8428d3b3eb097db1b6f2b4bcd7e
stella <mikkamakka89@gmail.com>
2022-04-02 12:11:57 +0200

    other solutions for the future

---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+console.log("Ez az első Javascriptem :)")
+/* ez a block comment
+// Ez a commentelés, a stringeket idézőjelekbe kell tenni.
+"Ez itt egy szöveg"
+// A dupla és sima macskakörmök csak 1 soros stringet engednek meg.
+'Ez itt egy másik szöveg'
+// ` a backtick a többsoros string-eket teszi lehetővé
+`Ez egy 
+több soros 
+szöveg`
+
+//a számokat nem kell idézőjelekbe tenni
+4
+
+128
+
+04
+
+1.13
+*/
+
+function myFirstFunction(){
+    console.log('Ez az első függvényem belsejében lévő log')
+}
+
+myFirstFunction()
